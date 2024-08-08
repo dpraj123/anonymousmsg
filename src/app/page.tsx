172da5471fb676443e656433ed9f07501b1fb482
dpraj123 <rajdp764@gmail.com>
2024-08-08 12:41:50 +0530

    theme switch addeded

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { ModeToggle } from "@/components/Mode";
+
 export default function Home() {
   return (
     <div className="p-5 dark:bg-black dark:text-white grid place-items-center h-[100vh]">
       <h1 className=" font-bold text-4xl font-mono text-center">
+        <ModeToggle />
         Welcome to New World...
       </h1>
     </div>
