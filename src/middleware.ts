340506fad77daeaf08afdbdc589a00e611dcb58f
dpraj123 <rajdp764@gmail.com>
2024-08-12 02:12:55 +0530

    backend almost complete

---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request });
+  const url = request.nextUrl;
+  if (
+    (token && url.pathname.startsWith("/sign-in")) ||
+    url.pathname.startsWith("/sign-up") ||
+    url.pathname.startsWith("/verify") ||
+    url.pathname.startsWith("/")
+  ) {
+    return NextResponse.redirect(new URL("/dashbord", request.url));
+  }
+  return NextResponse.redirect(new URL("/home", request.url));
+}
+
+export const config = {
+  matcher: ["/sing-in", "/sign-up", "/", "/dashboard/:path*"],
+};
